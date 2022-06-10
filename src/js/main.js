11a857f903d59e88b8c401e0e4b07b821577a69d
Yyyurii <yurii.kryvko@gmail.com>
2022-06-10 20:38:44 +0300

    Created function for send forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import './slider';
 import modal from './modules/modal';
 import tabs from './modules/tabs';
+import forms from './modules/forms';
 
 document.addEventListener('DOMContentLoaded', () => {
   modal();
   tabs();
+  forms();
 });
