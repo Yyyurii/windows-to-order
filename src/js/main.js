9cc866e0514ed673b2009dbee6eadb2d9214d17e
Yyyurii <yurii.kryvko@gmail.com>
2022-06-12 20:48:33 +0300

    Created timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,15 @@ import modal from './modules/modal';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 document.addEventListener('DOMContentLoaded', () => {
   let modalState = {};
+  let deadline = '2022-07-01';
 
   changeModalState(modalState);
   modal();
   tabs();
   forms(modalState);
+  timer('.container1', deadline);
 });
